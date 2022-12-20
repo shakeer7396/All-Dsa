799279e96606d92b551d3eb889ac632b62fda79e
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2022-12-20 16:28:32 +0530

    Create small,large.js

---
+++ Arrays/small,large.js
@@ -0,0 +1,64 @@
+// Smallest and Largest of all Ended
+// Description
+
+// Given an array,arrwhich hasNintegers.
+
+// You need to print the smallest and largest element present in the array each on new lines.
+
+
+// Input
+// Input Format
+
+// The first line contains an integerN.
+
+// The second line containsNspace separated integers, which are integers of arrayarr.
+
+// Constraints
+
+// 1 <= N <= 100
+
+
+// Output
+// Output Format
+
+// One first line, print minimum of all elements.
+
+// On the second line, print the maximum of all elements.
+
+
+// Sample Input 1 
+
+// 4
+// -2 0 8 4
+// Sample Output 1
+
+// -2
+// 8
+// Hint
+
+// Integers in array are:-[-2, 0, 8, 4]
+
+// Minimum integer in array[-2, 0, 8, 4]is-2
+
+// Maximum integer in array[-2, 0, 8, 4]is-8
+var arr=[-2,0,8,4];
+var n=arr.length-1;
+
+function small_large(n,arr){
+    var min=Infinity;
+    for(var i=0;i<n;i++){
+        if(arr[i]<min){
+            min=arr[i]
+        }
+    }
+
+    var max=-Infinity;
+    for(var i=0;i<n;i++){
+        if(arr[i]>max){
+            max=arr[i]
+        }
+    }
+    console.log(min);
+console.log(max)
+}
+
