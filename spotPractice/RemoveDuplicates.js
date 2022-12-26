6d217e3eecabbb721edea24ecf1ea2babb15445e
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2022-12-26 08:50:05 +0530

    Update RemoveDuplicates.js

--- spotPractice/RemoveDuplicates.js
+++ spotPractice/RemoveDuplicates.js
@@ -1,21 +1,38 @@
 var str='addcfcckaas'
 
-var obj={};
+// var obj={};
 
+// for(var i=0;i<str.length;i++){
+//     if(obj[str[i]]==undefined){
+//         obj[str[i]]=1;
+//     }
+//     else{
+//         obj[str[i]]+=1;
+//     }
+// }
+// // console.log(obj)
+// let bag="";
+// for(var key in obj){
+//     if(obj[key]<=1){
+//         bag+=key
+//     }
+// }
+// console.log(bag)
+
+var obj={};
 for(var i=0;i<str.length;i++){
     if(obj[str[i]]==undefined){
         obj[str[i]]=1;
     }
     else{
-        obj[str[i]]+=1;
+        obj[str[i]]+=1
     }
 }
 // console.log(obj)
 let bag="";
 for(var key in obj){
-    if(obj[key]<=1){
+    if(obj[key]>1){
         bag+=key
     }
 }
 console.log(bag)
-
