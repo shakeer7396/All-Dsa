630c955b2072040fcc3e1670e9ebd95e8926c272
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2023-02-16 23:03:44 +0530

    Update bubbleSort.js

--- BubbleSort/bubbleSort.js
+++ BubbleSort/bubbleSort.js
@@ -1,8 +1,15 @@
 var arr=[9,8,3,2,1,10,12];
 
 for(var i=0;i<arr.length;i++){
-    
+    for(var j=i+1;j<arr.length;j++){
+        if(arr[i]>arr[j]){
+            var temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+        }
+    }
 }
+console.log(arr.join(" "));
 
 // for(var i=0;i<arr.length;i++){
 //     for(var j=i+1;j<arr.length;j++){
