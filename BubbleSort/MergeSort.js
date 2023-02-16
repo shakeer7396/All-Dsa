a25554f65e01492463a46edf8990d6937f0f7726
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2023-02-16 22:26:32 +0530

    Update MergeSort.js

--- BubbleSort/MergeSort.js
+++ BubbleSort/MergeSort.js
@@ -13,3 +13,17 @@
 //     }
 // }
 // console.log(box)
+
+var arr1=[1,3,4,5,7,3,4,9,6,4];
+var arr2=[45,25,65,89,25,1,74,2];
+
+var box=[];
+
+for(var i=0;i<arr1.length;i++){
+    for(var j=0;j<arr2.length;j++){
+        if(arr1[i]>arr2[j]){
+            box.push(arr1[i])
+        }
+    }
+}
+console.log(box)
