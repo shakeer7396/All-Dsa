d60c0910d21dfe91fdb954e6a6f7ce53462f9e72
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2023-02-18 23:17:53 +0530

    Update SelectionSort.js

--- BubbleSort/SelectionSort.js
+++ BubbleSort/SelectionSort.js
@@ -15,21 +15,21 @@ console.log(arr.join(" "))
 // tc-0(n2)
 // sc-0(1)
 
-// function swap(arr,j,i){
-//     let swaap=arr[i];
-//     arr[i]=arr[index];
-//     arr[index]=swaap;
-// }
-// for(var i=0;i<arr.length;i++){
-//     index=i;
-//     for(var j=0;j<arr.length;j++){
-//         if(arr[j]>arr[index]){
-//             index=j;
-//         }
-//         swap(arr,j,i)
-//     }
-// }
-// console.log(arr.join(" "))
+function swap(arr,j,i){
+    let swaap=arr[i];
+    arr[i]=arr[index];
+    arr[index]=swaap;
+}
+for(var i=0;i<arr.length;i++){
+    index=i;
+    for(var j=0;j<arr.length;j++){
+        if(arr[j]>arr[index]){
+            index=j;
+        }
+        swap(arr,j,i)
+    }
+}
+console.log(arr.join(" "))
 
 // function swap(arr,j,i){
 //     let swaap=arr[i];
