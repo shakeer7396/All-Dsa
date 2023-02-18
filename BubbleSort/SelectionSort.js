19c84842711db8c79294969d57e3844c81b6acf8
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2023-02-18 23:16:21 +0530

    Update SelectionSort.js

--- BubbleSort/SelectionSort.js
+++ BubbleSort/SelectionSort.js
@@ -1,16 +1,16 @@
 var arr=[9,8,3,2,1,10,12,15,3];
-// for(var i=0;i<arr.length;i++){
-//     var index=i;
-//     for(var j=0;j<arr.length;j++){
-//         if(arr[j]>arr[index]){
-//             index=j;  
-//         }
-//         var temp=arr[i];
-//             arr[i]=arr[index]
-//             arr[index]=temp;
-//     }
-// }
-// console.log(arr.join(" "))
+for(var i=0;i<arr.length;i++){
+    var index=i;
+    for(var j=0;j<arr.length;j++){
+        if(arr[j]>arr[index]){
+            index=j;  
+        }
+        var temp=arr[i];
+            arr[i]=arr[index]
+            arr[index]=temp;
+    }
+}
+console.log(arr.join(" "))
 
 // tc-0(n2)
 // sc-0(1)
