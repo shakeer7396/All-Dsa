ca5f6e8300cb3d6539350c43e67980902680bc21
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2023-03-21 11:39:23 +0530

    Update ReverseArray.js

--- Venu sir pdf ques/ReverseArray.js
+++ Venu sir pdf ques/ReverseArray.js
@@ -30,30 +30,20 @@
 //     }
 // }
 // console.log(arr)
-for(var i=0;i<arr.length;i++){
-    for(var j=i+1;j<arr.length;j++){
-        if(arr[i]<arr[j]){
-            var temp=arr[i];
-            arr[i]=arr[j];
-            arr[j]=temp;
-        }
-    }
-}
-console.log(arr)
 // t-O(n2)
 // s-o(1)
 // ---------------------------------------------------
 // 2pointer
-// let left=0;
-// let right=arr.length-1;
-// while(left<right){
-//     let temp=arr[left];
-//     arr[left]=arr[right];
-//     arr[right]=temp
-//     left++;
-//     right--;
-// }
-// console.log(arr);
+let left=0;
+let right=arr.length-1;
+while(left<right){
+    let temp=arr[left];
+    arr[left]=arr[right];
+    arr[right]=temp
+    left++;
+    right--;
+}
+console.log(arr);
 
 // tc-o(n);
 // sc-o(1)
