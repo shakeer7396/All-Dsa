971a20ef872858624af2ac42febefb35b3284e87
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2023-04-04 19:50:45 +0530

    Create Multiples.js

---
+++ Basic Maths/Multiples.js
@@ -0,0 +1,18 @@
+var N=15;
+
+    for(var i=1;i<=N;i++){
+    if(N%5==0){
+        console.log("Masai")
+    }
+    else if(N%7==0){
+        console.log("School")
+    }
+    else if((N%5==0) && (N%7==0)){
+        console.log("MasaiSchool")
+    }
+    else{
+        console.log(i)
+    }
+}
+
+    
