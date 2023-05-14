d872e34930f98443ae451d38fbce37675c4e6449
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2023-05-14 22:00:40 +0530

    Create array.js

---
+++ mit/array.js
@@ -0,0 +1,26 @@
+/**
+ * [1,2,3,4,5,1,4] [1,4,5]
+ */
+var arr1=[1,2,3,4,5,1,4];
+var arr2=[1,4,5];
+// var bag=[];
+// for(var i=0;i<arr1.length;i++){
+//     for(var j=0;j<arr2.length;j++){
+//         if(arr1[i]==arr2[j]){
+//             bag.push(arr1[i])
+//         }
+//     }
+// }
+// let res=new Set(bag)
+// console.log(res) 
+
+var bag=[];
+for(var i=0;i<arr1.length;i++){
+    for(var j=0;j<arr2.length;j++){
+        if(arr1[i]==arr2[j]){
+            bag.push(arr1[i])
+        }
+    }
+}
+console.log(bag) 
+
