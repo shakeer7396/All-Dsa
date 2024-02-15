128730718551746ab6c51222d3c3cdd7e35ab848
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2024-02-15 13:22:51 +0530

    Create Remove duplicate.js

---
+++ Venu sir pdf ques/Remove duplicate.js
@@ -0,0 +1,7 @@
+// var arr=[1,2,2,4,3,3,5,6,6]
+// var uniqArr=arr.filter((item,index)=>arr.indexOf(item)===index);
+// console.log(uniqArr);
+
+var arr=[1,2,2,4,3,3,5,6,6]
+var unarr=Array.from(new Set(arr));
+console.log(unarr)
