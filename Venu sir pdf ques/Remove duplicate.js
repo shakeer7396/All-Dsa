365c2e8acd38ccd05ab8201a611823d045cae424
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2024-08-26 18:21:05 +0530

    placechange

--- Venu sir pdf ques/Remove duplicate.js
+++ Venu sir pdf ques/Remove duplicate.js
@@ -1,10 +1,24 @@
 
-var arr=[1,2,2,4,3,3,5,6,6]
-var uniqArr=arr.filter((item,index)=>arr.indexOf(item)===index);
-console.log(uniqArr);
+// var arr=[1,2,2,4,3,3,5,6,6]
+// var uniqArr=arr.filter((item,index)=>arr.indexOf(item)===index);
+// console.log(uniqArr);
 
 // var arr=[1,2,2,4,3,3,5,6,6]
 // var unarr=Array.from(new Set(arr));
 // console.log(unarr)
 
 
+var arr = [1, 2, 2, 2, 3, 4, 5, 5, 5, 5, 6, 7, 9];
+var uniqueObj = {};
+var uniqueArr = [];
+
+// Loop through the array
+for (var i = 0; i < arr.length; i++) {
+    // If the element is not already a key in the object, add it
+    if (!uniqueObj[arr[i]]) {
+        uniqueObj[arr[i]] = true;
+        uniqueArr.push(arr[i]);
+    }
+}
+
+console.log(uniqueArr);
