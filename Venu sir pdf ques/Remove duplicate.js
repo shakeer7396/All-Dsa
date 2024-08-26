06ba5160e90682074169ff06a3111a7c189cb7ac
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2024-08-26 18:23:43 +0530

    Update Remove duplicate.js

--- Venu sir pdf ques/Remove duplicate.js
+++ Venu sir pdf ques/Remove duplicate.js
@@ -22,3 +22,9 @@ for (var i = 0; i < arr.length; i++) {
 }
 
 console.log(uniqueArr);
+
+
+// var arr = [1, 2, 2, 2, 3, 4, 5, 5, 5, 5, 6, 7, 9];
+// var uniqueArr = [...new Set(arr)];
+
+// console.log(uniqueArr);
